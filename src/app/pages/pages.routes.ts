d4cb0d558ea7313a4a9269d1768cb170377968bd
enriketa.krriku <enienriketa@gmail.com>
2025-09-05 16:10:52 +0200

    add unneccessary components removal

--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -1,7 +1,3 @@
 import { Routes } from '@angular/router';
-import { Documentation } from './documentation/documentation';
-import { Crud } from './crud/crud';
-import { Empty } from './empty/empty';
-
 export default [
 ] as Routes;
