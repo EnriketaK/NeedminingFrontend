a3be709086ebcdfadf703be325f2328bdcbd5a72
enriketa.krriku <enienriketa@gmail.com>
2025-09-05 16:05:05 +0200

    add not needed paths removal

--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -4,8 +4,4 @@ import { Crud } from './crud/crud';
 import { Empty } from './empty/empty';
 
 export default [
-    { path: 'documentation', component: Documentation },
-    { path: 'crud', component: Crud },
-    { path: 'empty', component: Empty },
-    { path: '**', redirectTo: '/notfound' }
 ] as Routes;
